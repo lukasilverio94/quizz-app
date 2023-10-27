42e1ae96b50ef81b6d75316f8ced51319724349c
lukasilverio94 <lukasilverio94@gmail.com>
2023-10-27 23:48:51 +0200

    html and css added, just created js file and data

---
+++ script.js
@@ -0,0 +1,101 @@
+//Data
+const questions = [
+  {
+    question: "What does 'DOM' stand for in JavaScript?",
+    answers: [
+      { text: "Document Object Model", correct: true },
+      { text: "Data Object Model", correct: false },
+      { text: "Document Order Model", correct: false },
+      { text: "Dynamic Object Method", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which keyword is used to declare a constant variable in JavaScript?",
+    answers: [
+      { text: "var", correct: false },
+      { text: "int", correct: false },
+      { text: "let", correct: false },
+      { text: "const", correct: true },
+    ],
+  },
+  ,
+  {
+    question: "What is the result of 2 + '2' in JavaScript?",
+    answers: [
+      { text: "4", correct: false },
+      { text: "22", correct: true },
+      { text: "Error", correct: false },
+      { text: "Undefined", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which method is used to add elements to the end of an array in JavaScript?",
+    answers: [
+      { text: "push()", correct: true },
+      { text: "append()", correct: false },
+      { text: "addToEnd()", correct: false },
+      { text: "insert()", correct: false },
+    ],
+  },
+  {
+    question: "What does 'NaN' stand for in JavaScript?",
+    answers: [
+      { text: "Not a Number", correct: true },
+      { text: "Number and Null", correct: false },
+      { text: "Negative Absolute Number", correct: false },
+      { text: "No Action Needed", correct: false },
+    ],
+  },
+  {
+    question: "Which built-in object in JavaScript represents a date and time?",
+    answers: [
+      { text: "Math", correct: false },
+      { text: "Time", correct: false },
+      { text: "Date", correct: true },
+      { text: "Clock", correct: false },
+    ],
+  },
+  {
+    question: "What is the result of '5' == 5 in JavaScript?",
+    answers: [
+      { text: "true", correct: true },
+      { text: "false", correct: false },
+      { text: "Error", correct: false },
+      { text: "Undefined", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which JavaScript function is used to round a number to the nearest integer?",
+    answers: [
+      { text: "round()", correct: true },
+      { text: "ceil()", correct: false },
+      { text: "floor()", correct: false },
+      { text: "truncate()", correct: false },
+    ],
+  },
+  {
+    question:
+      "What is the purpose of the 'console.log()' function in JavaScript?",
+    answers: [
+      { text: "Display a pop-up alert", correct: false },
+      { text: "Print output to the console", correct: true },
+      { text: "Execute a function", correct: false },
+      { text: "Create a new variable", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which statement is used to exit a loop prematurely in JavaScript?",
+    answers: [
+      { text: "break", correct: true },
+      { text: "continue", correct: false },
+      { text: "return", correct: false },
+      { text: "exit", correct: false },
+    ],
+  },
+];
+
+// Variables
