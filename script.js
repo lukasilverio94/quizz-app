b573f2e866b6a8525d7501f208eda5b391be5354
lukasilverio94 <lukasilverio94@gmail.com>
2023-10-28 13:09:51 +0200

    added 5 more questions

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 //Data
 const questions = [
   {
+    question: "What is JavaScript used for?",
+    answers: [
+      { text: "Styling web pages", correct: false },
+      {
+        text: "Creating and controlling dynamic website content",
+        correct: true,
+      },
+      { text: "Managing server-side databases", correct: false },
+      { text: "None of the above", correct: false },
+    ],
+  },
+  {
     question: "What does 'DOM' stand for in JavaScript?",
     answers: [
       { text: "Document Object Model", correct: true },
@@ -96,6 +108,64 @@ const questions = [
       { text: "exit", correct: false },
     ],
   },
+  {
+    question:
+      "What is the purpose of the addEventListener method in JavaScript?",
+    answers: [
+      { text: "It adds an element to the web page.", correct: false },
+      { text: "It creates a new event in JavaScript.", correct: false },
+      { text: "It attaches an event handler to an element.", correct: true },
+      { text: "It removes an element from the web page.", correct: false },
+    ],
+  },
+  {
+    question: "How do you check the type of a variable in JavaScript?",
+    answers: [
+      { text: "typeof variableName", correct: true },
+      { text: "checkType(variableName)", correct: false },
+      { text: "variableName.type()", correct: false },
+      { text: "variableName.getType()", correct: false },
+    ],
+  },
+  {
+    question: "What is a closure in JavaScript?",
+    answers: [
+      {
+        text: "A built-in JavaScript function for closing browser tabs.",
+        correct: false,
+      },
+      { text: "A way to hide sensitive data in your code.", correct: false },
+      {
+        text: "A function that remembers the environment in which it was created, even after that environment is no longer available.",
+        correct: true,
+      },
+      {
+        text: "A method for opening and closing pop-up windows in web browsers.",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Explain the concept of 'callback functions' in JavaScript.",
+    answers: [
+      {
+        text: "Functions that are called automatically when a script is loaded.",
+        correct: false,
+      },
+      {
+        text: "Functions used to display alerts in the browser.",
+        correct: false,
+      },
+      {
+        text: "Functions passed as arguments to other functions and executed after a specific event or asynchronous operation.",
+        correct: true,
+      },
+      {
+        text: "Functions that return another function as their result.",
+        correct: false,
+      },
+    ],
+  },
 ];
 
 // Variables
@@ -107,7 +177,6 @@ const nextButton = document.getElementById("next-btn");
 let currentQuestionIndex = 0;
 let score = 0;
 
-// Functions
 // StartQuiz
 function startQuiz() {
   currentQuestionIndex = 0;
